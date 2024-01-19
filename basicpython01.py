da04bb2ef29f313debd9cde920784f218bb414b0
Amin Bista <aminbista6666@gmail.com>
2024-01-19 13:10:32 +0545

    class 1

---
+++ basicpython01.py
@@ -0,0 +1,38 @@
+"""
+(multi line comment)
+this is python class.
+"""
+
+"""
+Data types: 
+int, float, string
+
+array: collection of same data type
+int a[3] = {1, 2, 3};
+
+list, set, tuple
+"""
+# a = 2
+# print(a)
+# a = "Hello"
+# print(a)
+
+# a, b, c = 2, 1.23, "Hello"
+# print(a, b, c)
+
+'''
+list:list is a coolection of element that can also store duoplicate value
+a = [1, 2, 3, "Hello", 1.23, 2]
+print("a: ",a)
+'''
+
+'''
+set:set is a coolection of element that cannot also store duoplicate value
+b = {1, 2, 3, 4, 5, "Hello", 1.23, 2}
+print("b: ",b)
+'''
+
+c = (1, 2, 3, 4, "Hello", 1.23, 2)
+print(c)
+
+
