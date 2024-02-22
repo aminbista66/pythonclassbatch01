9ecd631f7e359fc106b985abb10d1937926a746a
Amin Bista <aminbista6666@gmail.com>
2024-02-22 16:43:48 +0545

    functions

---
+++ function.py
@@ -0,0 +1,54 @@
+def function_name(name):
+    print(f"My name is {name}")
+
+'''
+Q: Given an array of integer find the count of odd and even digits
+    arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+{
+    "even": 16,
+    "odd": 3
+}
+
+def find_odd_even_count(arr):
+'''
+
+
+def find_odd_even_count(arr):
+    result = {
+        "even": 0,
+        "odd": 0
+    }
+
+    for i in arr:
+        if i % 2 == 0:
+            result["even"] += 1
+        else:
+            result["odd"] += 1
+
+    print(result)
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# find_odd_even_count(arr)
+
+def fn_to_print():
+    for i in range(10):
+        print("Hello world")
+
+# function_name("Jhon doe")
+# fn_to_print()
+
+
+
+'''
+Q: find factorial of a number given by user
+'''
+
+def factorial(number):
+    result = 1
+    for i in range(1, number+1):
+        result *= i
+    return result, "Hello World", 89
+
+
+fact, string, number = factorial(5)
---
+++ loop.py
@@ -0,0 +1,39 @@
+'''
+for -> array or range
+
+'''
+
+# arr = [1, 3, 4, 5, 6, 7, 8, 9]
+
+''' in operator '''
+# for i in arr:
+#     print(i)
+
+''' range function '''
+# for i in range(4, 8):
+#     print(arr[i])
+
+
+''' while function '''
+# counter = 0
+
+# while counter < len(arr):
+#     if arr[counter] % 2 == 0:
+#         continue
+#     print(arr[counter])
+#     counter += 1
+
+
+'''
+Q: Take a number as input and display the sum of number from 1 to that number
+
+'''
+
+user_input = int(input("Enter a number: "))
+
+result = 0
+
+for i in range(1, user_input + 1):
+    result += i
+
+print(result)
